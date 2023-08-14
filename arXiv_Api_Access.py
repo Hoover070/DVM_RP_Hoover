5dfbcbec68ba533158fb1a4340954ae3c470e061
Whoover <willhooverfilm@gmail.com>
2023-08-14 15:37:53 -0400

    the webscrape API is working and retrieving dates or AI research. concept proven, moving on to pull ML papers and dates, DL, and others to compare to eachother over the same amount of time.

--- arXiv_Api_Access.py
+++ arXiv_Api_Access.py
@@ -5,42 +5,55 @@ import time
 
 """
 This is the arXiv API access code for the research project to DVM for Hoover, William
+
+Args: None
+returns: Data Frame with freshly scraped data from arXiv API
 """
 
-# Variables
-saved_papers = []
-start_index = 0
-max_results = 100
 
-# define the query parameters of the search. AI: Artificial Intelligence JAN012010-DEC312020
-while True:
-    query_params = {
-        'search_query': 'all:artificial intelligence AND submittedDate:[20100101 TO 20201231]',
-        'start': 0, #start_index,
-        'max_results': 99, #max_results  # max is 100
-    }
+def scrape_arxiv():
 
-    # send the access request to the arXiv API
-    response = requests.get('http://export.arxiv.org/api/query', params=query_params)
-    soup = BeautifulSoup(response.content, 'xml')
+    # Variables
+    saved_papers = []
+    start_index = 0
+    max_results = 100
 
-    # extract and save the data
-    entries = soup.find_all('entry')
-    for entry in entries:
-        title = entry.title
-        year = entry.published
-        affiliations = entry.affiliation
-        saved_papers.append([title, year, affiliations])
+    # define the query parameters of the search. AI: Artificial Intelligence JAN012010-DEC312020
+    while True:
+        query_params = {
+            'search_query': 'all:artificial intelligence AND submittedDate:[20000101 TO 20231231]',
+            'start':  start_index,
+            'max_results': max_results  # max is 100
+        }
 
-    if len(entries) < max_results:
-        break
+        # send the access request to the arXiv API
+        response = requests.get('http://export.arxiv.org/api/query', params=query_params)
+        soup = BeautifulSoup(response.content, 'xml')
 
-    start_index += max_results
-    time.sleep(3)  # Pause to be considerate, they advise 2 seconds so i added 1 more
+        # extract and save the data
+        entries = soup.find_all('entry')
+        for entry in entries:
+            title = entry.title.text
+            year = entry.published.text[:4]
+            affiliations_tag = entry.find('affiliation')
+            affiliations = affiliations_tag.text if affiliations_tag else 'Independent'
+            saved_papers.append([title, year, affiliations])
+        if len(entries) < max_results:
+            break
 
-# Create a DataFrame
-df = pd.DataFrame(saved_papers, columns=['Title', 'Year', 'Affiliations'])
+        start_index += max_results
+        time.sleep(3)  # Pause to be considerate, they advise 2 seconds so i added 1 more
 
+    # Create a DataFrame
+    df = pd.DataFrame(saved_papers, columns=['Title', 'Year', 'Affiliations'])
+
+    # convert to int
+    df['Year'] = df['Year'].astype(int)
+
+    # save dataframe df as CSV file
+    df.to_csv('ai_research_data.csv', index=False)
+
+    return df
 
 
 
--- main.py
+++ main.py
@@ -6,3 +6,22 @@ over the course of a 10-year period, to track the growth of the field and to see
 if research has been speeding up or slowing down
 """
 
+#imports
+from arXiv_Api_Access import scrape_arxiv
+import matplotlib.pyplot as plt
+
+
+#variables
+data_frame = scrape_arxiv()
+
+
+#number of total AI papers published
+data_frame['Year'].value_counts().sort_index().plot(kind='bar')
+plt.title('Number of AI Papers Published per Year')
+plt.xlabel('Year')
+plt.ylabel('Number of Papers')
+plt.savefig('Number of AI Papers Published per Year.png')
+plt.show()
+
+
+
